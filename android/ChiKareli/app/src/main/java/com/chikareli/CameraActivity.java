719feb6bfb6cfbae1637f41707ce6843f2768445
Tigran Balayan <balayan.tigran@gmail.com>
2016-02-04 02:28:46 +0400

    Add media directories and file saving
      - add image button
      - change layout styles

--- android/ChiKareli/app/src/main/java/com/chikareli/CameraActivity.java
+++ android/ChiKareli/app/src/main/java/com/chikareli/CameraActivity.java
@@ -8,67 +8,110 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class CameraActivity extends Activity {
-    private static String TAG = "CAMERA_LOGS:";
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
-
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    private static String TAG = "CAMERA_LOGS:";
     private Uri fileUri;
+
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File setUpDirs(int type) {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "ChiKareli");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "failed to create directory");
+                return null;
+            }
+        }
+        if (type == MEDIA_TYPE_IMAGE) {
+            File imgDir = new File(String.valueOf(mediaStorageDir), "image");
+            if (!imgDir.exists()) {
+                if (!imgDir.mkdirs()) {
+                    Log.d(TAG, "failed to create directory");
+                    return null;
+                }
+            }
+            return imgDir;
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            File videoDir = new File(String.valueOf(mediaStorageDir), "video");
+            if (!videoDir.exists()) {
+                if (!videoDir.mkdirs()) {
+                    Log.d(TAG, "failed to create directory");
+                    return null;
+                }
+            }
+            return videoDir;
+        } else {
+            return null;
+        }
+    }
+
+    private static File getOutputMediaFile(int type) {
+        File mediaStorageDir = setUpDirs(type);
+        String timeStamp = new SimpleDateFormat("dd_MM_yyyy_HH.mm").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+        return mediaFile;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-            new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startCamera();
-                }
-            }
-        );
+        setUpEventListeners();
     }
 
-    private void startCamera() {
+    private void setUpEventListeners() {
+        ImageButton img = (ImageButton) findViewById(R.id.image_capture);
+        setImgBtnListener(img, "img");
+        ImageButton video = (ImageButton) findViewById(R.id.video_capture);
+        setImgBtnListener(video, "video");
+    }
+
+    private void setImgBtnListener(ImageButton btn, final String id) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (id == "img") {
+                    startCameraImage();
+                } else if (id == "video") {
+                    startCameraVideo();
+                }
+            }
+        });
+    }
+
+    private void startCameraImage() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
         startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
 
-    private static Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    private static File getOutputMediaFile(int type){
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                Log.d(TAG, "failed to create directory");
-                return null;
-            }
-        }
-
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE){
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_"+ timeStamp + ".jpg");
-        } else if(type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_"+ timeStamp + ".mp4");
-        } else {
-            return null;
-        }
-        return mediaFile;
+    private void startCameraVideo() {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO); // create a file to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+        startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
     }
 }

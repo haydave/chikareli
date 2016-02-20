0c02b2f1ed25634c3ce2c28a41ceb88877688a79
David <davidhayriyan@gmail.com>
2016-02-20 21:16:13 +0400

    Added initial version for file uploading (test) 127.0.0.1:8000/list

--- web/chikareli/chikareli/settings.py
+++ web/chikareli/chikareli/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -114,6 +115,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
--- web/chikareli/website/models.py
+++ web/chikareli/website/models.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')

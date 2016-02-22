f5f469dc8ef796892979c01337f3b1318f44b944
David <davidhayriyan@gmail.com>
2016-02-22 23:58:41 +0400

    Added minor image test check

--- web/chikareli/website/views.py
+++ web/chikareli/website/views.py
@@ -12,10 +12,12 @@ def list(request):
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
+        if request.FILES['docfile'].name.lower().endswith(('.png', '.jpg', '.jpeg')):
+            print "lava"
         if form.is_valid():
             newdoc = Document(docfile=request.FILES['docfile'])
             newdoc.save()
-            # Redirect to the document list after POST
+            # Redirect to the document list after POST 'Hin method e 1.10-um jnjvelu e website.views.index
             return HttpResponseRedirect(reverse('website.views.index'))
     else:
         form = DocumentForm()  # A empty, unbound form
@@ -27,6 +29,7 @@ def list(request):
     return render_to_response(
         'list.html',
         {'documents': documents, 'form': form},
+        #  'Hin method e 1.10-um jnjvelu e website.views.inde
         context_instance=RequestContext(request)
     )
 
